538e5a2709b3a8d66252c2ef4822296f755c9ac7
Shashank Neela <shankuneela@Shankus-MBP.mshome.net>
2024-04-27 01:40:51 +0530

    27april24-140

--- OnlineFoodOrdering/settings.py
+++ OnlineFoodOrdering/settings.py
@@ -75,25 +75,25 @@ WSGI_APPLICATION = 'OnlineFoodOrdering.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
 
-DATABASES={
-    'default':{
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'OnlineFood',
-        'USER':'postgres',
-        'PASSWORD':'2841',
-        'HOST':'localhost',
-        'PORT':'2841'
-    }
-}
+
+# DATABASES={
+#     'default':{
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':'OnlineFood',
+#         'USER':'postgres',
+#         'PASSWORD':'2841',
+#         'HOST':'localhost',
+#         'PORT':'2841'
+#     }
+# }
 
 
 # Password validation
